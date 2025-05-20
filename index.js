da629afb66a42bed26cb6b1a09e471ca7347a86d
xuannhj <xuannhi.nguyenpham@gmail.com>
2025-05-20 14:37:10 +0700

    mockapi & axios

--- index.js
+++ index.js
@@ -12,11 +12,53 @@ document.addEventListener("DOMContentLoaded", getTodos);
 
 // }
 
-// async là từ khóa để khai báo hàm bất đồng bộ
 async function getTodos() {
-    try {
-        const response = await fetch("https://68272e946b7628c5290f5ba4.mockapi.io/tasks");
-    }
-    
-}
+    // biến nó thành 1 hàm đồng bộ
+    // await là chờ cho hàm này chạy xong rồi mới chạy tiếp
+    // async và await lun đi chung với nhaunhau
+    try{
 
+    const response = await axios.get ("https://68272e946b7628c5290f5ba4.mockapi.io/tasks");
+    console.log(response.data);
+    response.data.forEach((item) => {
+    // forEach là 1 hàm lặp qua từng phần tử trong mảng
+    const date = new Date(item.createdAt);
+    // createdAt là 1 thuộc tính trong 
+    const formatDate = `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
+    // đặt biến vào chuỗi
+    // cái chuỗi bình thường thì không thể gán biến vào được
+    // nhưng nếu mà có dấu ` thì có thể gán biến vào được
+
+    // Tạo li 
+    const li = document.createElement("li");
+    //  gắn class to do item để nó ăn css
+    li.className = "todo-item";
+    // gắn thắng con và nội dung từ api cho thăng con
+    li.innerHTML = ` <div class = "todo-content">
+
+                        <input type="checkbox">
+                        <div>
+                            <span>${item.content}
+                            </span>
+                            <p > Created: ${formatDate} </span>
+                            <!-- dung the p, xem lai inline va block -->
+                        </div>
+                    </div>
+
+
+                    <li class = "todo-item"></li>
+    
+
+                    <div class = "todo-actions">
+                    <button> <i class="fa-solid fa-pen-to-square"></i></button>
+                    <button> <i class="fa-solid fa-trash"></i></button>
+                    </div>`;
+    // gắn vào ul
+    const ul = document.querySelector(".todo-list");
+    ul.appendChild(li);
+    });
+    }
+    catch (error) {
+        console.log("Thất bại òi" + error);
+    }
+}
